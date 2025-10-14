e2b5767431abfa57a71dcde920d4a99ad9948b8b
tessavermeulen07 <tessavermeulen@me.com>
2025-10-14 15:31:01 +0200

    start met loops.js

--- loops.js
+++ loops.js
@@ -10,6 +10,9 @@
 // Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen... Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
+for (let i = 0; i < 6; i++) {
+
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de laatste (vijfde) loop het woord "klaar!"
